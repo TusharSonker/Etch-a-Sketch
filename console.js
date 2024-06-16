4160444aa8a3716c9a34b82beb39d2f4be996569
Tushar Sonker <tusharsonker2962004@gmail.com>
2024-06-16 20:53:01 +0530

    Give functionality to input and button tag
    Added a feature which takes user's input and after clicking the
    generate button, generates a grid based on user's input.

--- console.js
+++ console.js
@@ -1,14 +1,22 @@
-const container = document.querySelector('.container');
-for(let i = 0;i<16;i++) {
-    let parent = document.createElement('div.parent');
-    for(let j = 0;j<16;j++) {
-        let child = document.createElement('div');
-        child.setAttribute(
-            'style','height: 20px; width: 20px; border: 2px solid red'
-        );
-        child.addEventListener('click',()=>{child.style.backgroundColor = 'red';});
-        parent.appendChild(child);
+const button = document.querySelector('button');
+button.addEventListener('click',()=>{generateGrid();});
+
+function generateGrid() {
+    const input = document.querySelector('input');
+    let inputValue = input.value;
+    console.log(inputValue)
+    const container = document.querySelector('.container');
+    for(let i = 0;i<inputValue;i++) {
+        let parent = document.createElement('div.parent');
+        for(let j = 0;j<inputValue;j++) {
+            let child = document.createElement('div');
+            child.setAttribute(
+                'style','height: 20px; width: 20px; border: 2px solid red'
+            );
+            child.addEventListener('click',()=>{child.style.backgroundColor = 'red';});
+            parent.appendChild(child);
+        }
+        parent.setAttribute('style','display: flex;');
+        container.appendChild(parent);
     }
-    parent.setAttribute('style','display: flex;');
-    container.appendChild(parent);
 }
